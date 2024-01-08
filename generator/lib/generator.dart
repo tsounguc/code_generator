0575cbe780936546ed6fa60762fb1f263f02f252
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-08 13:39:55 -0500

    Create generateJsonClass() in generator/lib/generator.dart file

--- generator/lib/generator.dart
+++ generator/lib/generator.dart
@@ -1,7 +1,10 @@
 library generator;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+import 'package:build/build.dart';
+import 'package:generator/src/json_generator.dart';
+import 'package:source_gen/source_gen.dart';
+
+Builder generateJsonClass(BuilderOptions options) => SharedPartBuilder(
+      [JsonGenerator()],
+      'json_generator',
+    );
